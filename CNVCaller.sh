f33fa82b9e450d9536fe22918a5b59dce51fd032
Lulu Shi <2114563331@qq.com>
2023-03-05 17:38:45 +0800

    Update CNVCaller.sh

--- CNVCaller.sh
+++ CNVCaller.sh
@@ -1,15 +1,28 @@
+#! /bin/bash
+################ CNVCaller for genome data ##############
+# Run this program in your bam file directory
+# The format of the bam file should be .sorted.addhead.markdup.bam
 
-# 对基因组创建窗口文件（以后直接用即可）
-perl /home/sll/miniconda3/CNVcaller/bin/CNVReferenceDB.pl /home/sll/genome-cattle/ARS-UCD1.2/GCF_002263795.1_ARS-UCD1.2_genomic.fna -w 800
-# 计算每个窗口的绝对拷贝数
+# Set up the file name(obtain the absolute paths), software
+CNVReferenceDB.pl="/home/sll/miniconda3/CNVcaller/bin/CNVReferenceDB.pl"                  #change as you want
+genomic.fna="/home/sll/genome-cattle/ARS-UCD1.2/GCF_002263795.1_ARS-UCD1.2_genomic.fna"   #Reference genome fa file, change as you want
+Individual.Process.sh="/home/sll/miniconda3/CNVcaller/Individual.Process.sh"              #change as you want
+Btau5.0.1_800_link="/home/sll/miniconda3/CNVcaller/Btau5.0.1_800_link"                    #dup file that you have created use blasr, change as you want
+CNV.Discovery.sh="/home/sll/miniconda3/CNVcaller/CNV.Discovery.sh"                        #change as you want
+Genotype.py="/home/sll/miniconda3/CNVcaller/Genotype.py"                                  #change as you want
+
+# Create a window file for the genome (you can use it directly later)
+perl CNVReferenceDB.pl genomic.fna -w 800
+
+# Calculate the absolute copy number  of each window
 ls *bam|cut -d"." -f 1 | sort -u | while read id; 
-do bash /home/sll/miniconda3/CNVcaller/Individual.Process.sh -b `pwd`/${id}.sorted.addhead.markdup.bam -h ${id} -d /home/sll/miniconda3/CNVcaller/Btau5.0.1_800_link -s none; 
+do bash Individual.Process.sh -b `pwd`/${id}.sorted.addhead.markdup.bam -h ${id} -d Btau5.0.1_800_link -s none; 
 done
 
-# 将referenceDB.800文件复制到RD_normalized目录下
+# Copy the file referenceDB.800 to directly RD_normalized
 cp referenceDB.800 RD_normalized
 
-# 进入RD_normalized目录
+# Enter the directly RD_normalized
 cd RD_normalized
 
 # 将RD_normalized目录下新生成的sex_1结尾的文件名，以绝对路径的形式写入list.txt中
@@ -19,7 +32,7 @@ ls -R `pwd`/*sex_1 > list.txt
 touch exclude_list
 
 # 拷贝数变异区域的确定
-bash /home/sll/miniconda3/CNVcaller/CNV.Discovery.sh -l `pwd`/list.txt -e `pwd`/exclude_list -f 0.1 -h 1 -r 0.1 -p primaryCNVR -m mergeCNVR
+bash CNV.Discovery.sh -l `pwd`/list.txt -e `pwd`/exclude_list -f 0.1 -h 1 -r 0.1 -p primaryCNVR -m mergeCNVR
 
 # 基因型判定
-python /home/sll/miniconda3/CNVcaller/Genotype.py --cnvfile mergeCNVR --outprefix genotypeCNVR --nproc 8
+python Genotype.py --cnvfile mergeCNVR --outprefix genotypeCNVR --nproc 8
