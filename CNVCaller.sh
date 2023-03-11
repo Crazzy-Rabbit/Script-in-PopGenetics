4a1f0b494317a07be96a8310677a873354d786d4
Lulu Shi <2114563331@qq.com>
2023-03-11 21:19:51 +0800

    Update CNVCaller.sh

--- CNVCaller.sh
+++ CNVCaller.sh
@@ -7,33 +7,37 @@
 
 # Set up the file name(obtain the absolute paths), software
 CNVReferenceDB.pl="/home/sll/miniconda3/CNVcaller/bin/CNVReferenceDB.pl"                  #change as you want
-genomic.fna="/home/sll/genome-cattle/ARS-UCD1.2/GCF_002263795.1_ARS-UCD1.2_genomic.fna"   #Reference genome fa file, change as you want
 Individual.Process.sh="/home/sll/miniconda3/CNVcaller/Individual.Process.sh"              #change as you want
-Btau5.0.1_800_link="/home/sll/miniconda3/CNVcaller/Btau5.0.1_800_link"                    #dup file that you have created use blasr, change as you want
 CNV.Discovery.sh="/home/sll/miniconda3/CNVcaller/CNV.Discovery.sh"                        #change as you want
 Genotype.py="/home/sll/miniconda3/CNVcaller/Genotype.py"                                  #change as you want
+genomic.fna="/home/sll/genome-cattle/ARS-UCD1.2/GCF_002263795.1_ARS-UCD1.2_genomic.fna"   #Reference genome fa file, change as you want
+Btau5.0.1_800_link="/home/sll/miniconda3/CNVcaller/Btau5.0.1_800_link"                    #dup file that you have created use blasr, change as you want
+
+echo $CNVReferenceDB.pl
+echo $genomic.fna
+echo $Individual.Process.sh
+echo $Btau5.0.1_800_link
+echo $CNV.Discovery.sh
+echo $Genotype.py
 
 # Create a window file for the genome (you can use it directly later)
-perl ${CNVReferenceDB.pl} ${genomic.fna} -w 800
+perl $CNVReferenceDB.pl $genomic.fna -w 800
 
 # Calculate the absolute copy number  of each window
-bam=`ls *bam|cut -d"." -f 1`
+bam=`ls *bam|cut -d"." -f 1 | sort -u`
 for i in $bam
 do 
-bash ${Individual.Process.sh} -b `pwd`/${bam}.sorted.addhead.markdup.bam -h ${id} -d ${Btau5.0.1_800_link} -s none;
+bash $Individual.Process.sh -b `pwd`/$bam.sorted.addhead.markdup.bam -h $bam -d $Btau5.0.1_800_link -s none;
 done    
 
-# Copy the file referenceDB.800 to directly RD_normalized
 cp referenceDB.800 RD_normalized
-# Enter the directly RD_normalized
 cd RD_normalized
-# Write  *.sex_1 in the RD_normalized directory to list.txt as  absolute path
 ls -R `pwd`/*sex_1 > list.txt
-# Creat a exclude_list file
 touch exclude_list
 
 # Determin the CNV region
-bash ${CNV.Discovery.sh} -l `pwd`/list.txt -e `pwd`/exclude_list -f 0.1 -h 1 -r 0.1 -p primaryCNVR -m mergeCNVR
+bash $CNV.Discovery.sh -l `pwd`/list.txt -e `pwd`/exclude_list -f 0.1 -h 1 -r 0.1 -p primaryCNVR -m mergeCNVR
 
 # Genotype determination
-python ${Genotype.py} --cnvfile mergeCNVR --outprefix genotypeCNVR --nproc 8
+python $Genotype.py --cnvfile mergeCNVR --outprefix genotypeCNVR --nproc 8
+echo "CNVCaller finished!"
