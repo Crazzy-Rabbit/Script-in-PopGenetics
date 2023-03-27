9d9d376fc309e80c4e95cac0f7be9f73f0b1c126
Lulu Shi <crazzy_rabbit@163.com>
2023-03-27 17:00:52 +0800

    Update 01_DupfiletoCNVCaller.sh

--- CNVcaller/01_DupfiletoCNVCaller.sh
+++ CNVcaller/01_DupfiletoCNVCaller.sh
@@ -4,15 +4,15 @@
 # 400 in this script is windowsize that you want to set in dup file, change as you want in step 1 and 3
 
 
-# Set up the file name(obtain the absolute paths), software
+# Set up the file name(obtain the absolute paths), software and winsize 
 0.1.Kmer_Generate.py="/home/sll/miniconda3/CNVcaller/bin/0.1.Kmer_Generate.py"                #change as you want
 genomic.fna="/home/sll/genome-cattle/ARS-UCD1.2/GCF_002263795.1_ARS-UCD1.2_genomic.fna"       #change as you want
 sawriter="/home/sll/software/blasr/alignment/bin/SAWriter"                                    #change as you want
 blasr="/home/sll/software/blasr/alignment/Blasr"                                              #change as you want
 0.2.Kmer_Link.py="/home/sll/miniconda3/CNVcaller/bin/0.2.Kmer_Link.py"                        #change as you want
-
+winsize=1000                                                                                  #change as you want
 # 1 Split genome into short kmer sequences
-python ${0.1.Kmer_Generate.py} ${genomic.fna} 400 kmer.fa
+python $0.1.Kmer_Generate.py $genomic.fna $winsize kmer.fa
 
 # <FAFILE>      Reference sequence in FASTA format
 # <WINSIZE>     The size of the window to use for CNV calling
@@ -20,15 +20,15 @@ python ${0.1.Kmer_Generate.py} ${genomic.fna} 400 kmer.fa
 
 # 2 Align the kmer FASTA (from step 1) to reference genome using blasr.
 # 1) creat .sa file use sawriter
-${sawriter} genomic.fna.sa ${genomic.fna}
+$sawriter genomic.fna.sa $genomic.fna
 # 2) blasr 
-${blasr} kmer.fa ${genomic.fna} --sa genomic.fna.sa \
-                                --out kmer.aln -m 5 --noSplitSubreads --minMatch 15 --maxMatch 20 \
-                                --advanceHalf --advanceExactMatches 10 --fastMaxInterval \
-                                --fastSDP --aggressiveIntervalCut --bestn 10
+$blasr kmer.fa $genomic.fna --sa genomic.fna.sa \
+                            --out kmer.aln -m 5 --noSplitSubreads --minMatch 15 --maxMatch 20 \
+                            --advanceHalf --advanceExactMatches 10 --fastMaxInterval \
+                            --fastSDP --aggressiveIntervalCut --bestn 10
 
 # 3 Generate duplicated window record file
-python ${0.2.Kmer_Link.py} kmer.aln 400 Bos_ARS1.2_window.link
+python $0.2.Kmer_Link.py kmer.aln $winsize Bos_ARS1.2_${winsize}.link
 
 # <BLASR>       blasr results (-m 5 format)
 # <WINSIZE>     The size of the window to use for CNV calling
