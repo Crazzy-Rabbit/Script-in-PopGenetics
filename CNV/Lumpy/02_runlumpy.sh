7a3b1a5ec5c445915ba15d4dd757b0d40c1b8004
Lulu Shi <crazzy_rabbit@163.com>
2023-04-10 17:01:45 +0800

    Update 02_runlumpy.sh

--- CNV/Lumpy/02_runlumpy.sh
+++ CNV/Lumpy/02_runlumpy.sh
@@ -6,20 +6,19 @@ lumpyexpress \
 -o all.sv.lumpy.vcf
 
 
-sample=`ls *.markdup.bam | cut -d '.' -f 1 | sort -u`
-for i in $sample;
+ls *.markdup.bam | cut -d '.' -f 1 | sort -u | while read id;
 do 
 # 2.提取样本
 vcftools --vcf all.sv.lumpy.vcf \ #指定提取样品名称
---indv $sample --recode --recode-INFO-all
---out $sample  #输出文件前缀
+--indv $id --recode --recode-INFO-all
+--out $id  #输出文件前缀
 
 # 2. genotype
 svtyper \
--B ${sample}.sorted.addhead.markdup.bam \
--S ${sample}.splitters.bam \
--i ${sample}.recode.vcf
-> ${sample}.gt.vcf
+-B ${id}.sorted.addhead.markdup.bam \
+-S ${id}.splitters.bam \
+-i ${id}.recode.vcf
+> ${id}.gt.vcf
 done
 
 # 3 merge samples
