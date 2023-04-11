60374817b2e78184dcc4821d3184609f76ead426
Lulu Shi <crazzy_rabbit@163.com>
2023-04-11 21:13:27 +0800

    Create ROH.sh

---
+++ Genome_Diversity/ROH.sh
@@ -0,0 +1,4 @@
+plink --homozyg --homozyg-window-snp 50 --homozyg-density 50 --homozyg-window-het 3 --homozyg-window-missing 5 --out
+
+#.indiv文件中NSEG为个体中ROH的个数，KB为长度，KBAVG为平均长度。
+#计数时使用.hom文件，按区间统计
