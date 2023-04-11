949e78dd161458d71e37ec6c579645f97c0f1163
Lulu Shi <crazzy_rabbit@163.com>
2023-04-11 21:26:38 +0800

    Update 01_cal-Admixture.sh

--- Admixture/01_cal-Admixture.sh
+++ Admixture/01_cal-Admixture.sh
@@ -1,3 +1,7 @@
 for K in 2 3 4 5 6 7 8 9 10 11 12 13 14 15; 
 do /home/software/admixture_linux-1.3.0/admixture --cv QC.sample-northeast_Asia-geno005-maf003.bed $K | tee log${K}.out;
 done
+
+
+# CV error最小的为最佳K值
+grep -h CV log*.out
