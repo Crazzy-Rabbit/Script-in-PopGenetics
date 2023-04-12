3ddc6e05b75ad7bccc5454dbebbe108ad43ea6fe
Lulu Shi <crazzy_rabbit@163.com>
2023-04-12 10:20:03 +0800

    Create plot_CNVmap.sh

---
+++ CNV/plot_CNVmap.sh
@@ -0,0 +1,4 @@
+# in.cofi文件中修改File1  = ./chat.rectchr 为自己的输入文件即可
+# chat.rectchr 文件格式为 chr start end distance type五列
+/home/sll/software/RectChr/bin/RectChr -InConfi in.cofi \
+                                       -OutPut OUT
