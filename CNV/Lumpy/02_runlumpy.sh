bd656cb7ba2f25b1364de3e1272f820442de3a25
Lulu Shi <crazzy_rabbit@163.com>
2023-04-12 15:28:05 +0800

    Update 02_runlumpy.sh

--- CNV/Lumpy/02_runlumpy.sh
+++ CNV/Lumpy/02_runlumpy.sh
@@ -1,8 +1,8 @@
 # 1.run lumpy
 lumpyexpress \
--B ReiD-18.sorted.addhead.markdup.bam,ReiD-19.sorted.addhead.markdup.bam\ #指定输入文件,可以多个样品(逗号隔开)
--S ReiD-18.splitters.bam,ReiD-19.splitters.bam \             #指定分裂比对的文件，可以多个样品
--D ReiD-18.discordants.bam,ReiD-18.discordants.bam \           #指定不正常比对文件，可以多个样品 
+-B ReiD-18.sorted.addhead.markdup.bam,ReiD-19.sorted.addhead.markdup.bam \ # 指定输入文件,可以多个样品(逗号隔开)
+-S ReiD-18.splitters.bam,ReiD-19.splitters.bam \             # 指定分裂比对的文件，可以多个样品
+-D ReiD-18.discordants.bam,ReiD-18.discordants.bam \           # 指定不正常比对文件，可以多个样品 
 -o all.sv.lumpy.vcf
 
 
@@ -10,18 +10,18 @@ ls *.markdup.bam | cut -d '.' -f 1 | sort -u | while read id;
 do 
 # 2.提取样本
 vcftools --vcf all.sv.lumpy.vcf \ #指定提取样品名称
---indv $id --recode --recode-INFO-all
+--indv $id --recode --recode-INFO-all \
 --out $id  #输出文件前缀
 
 # 2. genotype
 svtyper \
 -B ${id}.sorted.addhead.markdup.bam \
 -S ${id}.splitters.bam \
--i ${id}.recode.vcf
+-i ${id}.recode.vcf \
 > ${id}.gt.vcf
 done
 
 # 3 merge samples
 ls *gt.vcf > vcf.list
 
-/home/sll/miniconda3/envs/python2.7/bin/svtools vcfpaste -f  vcf.list  > all.genotype.vcf
+svtools vcfpaste -f  vcf.list  > all.genotype.vcf
