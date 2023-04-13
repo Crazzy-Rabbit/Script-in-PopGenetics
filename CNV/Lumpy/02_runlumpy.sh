a942f4d12e50419eec68706919d750a59b780de5
Lulu Shi <crazzy_rabbit@163.com>
2023-04-13 09:18:10 +0800

    Update 02_runlumpy.sh

--- CNV/Lumpy/02_runlumpy.sh
+++ CNV/Lumpy/02_runlumpy.sh
@@ -3,30 +3,10 @@
 source /home/sll/miniconda3/bin/activate
 conda activate python2.7
 
+# 需要提供bam文件的bai索引文件
 # 1.run lumpy
 lumpyexpress \
 -B /home/sll/2023-deer-study/20230405-deer/ReiD-18-bam/ReiD-18.sorted.addhead.markdup.bam,/home/sll/2023-deer-study/20230405-deer/ReiD-19-bam/ReiD-19.sorted.addhead.markdup.bam \ # 指定输入文件,可以多个样品
 -S ReiD-18.splitters.bam,ReiD-19.splitters.bam \             # 指定分裂比对的文件，可以多个样品
 -D ReiD-18.discordants.bam,ReiD-18.discordants.bam \           # 指定不正常比对文件，可以多个样品 
 -o all.sv.lumpy.vcf
-
-
-ls *.markdup.bam | cut -d '.' -f 1 | sort -u | while read id;
-do 
-# 2.提取样本
-vcftools --vcf all.sv.lumpy.vcf \ #指定提取样品名称
-         --indv $id --recode --recode-INFO-all \
-         --out $id  #输出文件前缀
-
-# 2. genotype
-svtyper \
--B ${id}.sorted.addhead.markdup.bam \
--S ${id}.splitters.bam \
--i ${id}.recode.vcf \
-> ${id}.gt.vcf
-done
-
-# 3 merge samples
-ls *gt.vcf > vcf.list
-
-svtools vcfpaste -f  vcf.list  > all.genotype.vcf
