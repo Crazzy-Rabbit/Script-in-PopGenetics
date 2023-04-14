4032715a3c4063579e8eb74bbe160e6def72d9af
Lulu Shi <crazzy_rabbit@163.com>
2023-04-14 15:31:40 +0800

    Create 02.treemix.pl

---
+++ Gene_flow/Treemix/02.treemix.pl
@@ -0,0 +1,26 @@
+#! /usr/bin/env perl
+use strict;
+use warnings;
+
+my $file="noBuffalo.gz"; # 上一步转换后的输入文件
+my $output_dir="twentyOne"; # 输出目录
+my $root="cattle"; # 外群
+`mkdir $output_dir` if(!-e "$output_dir");
+
+open(O1,"> run_treemix.sh");
+open(O2,"> run_plot.Rscript");
+for(my $i=0;$i<5;$i++){
+    my $m_para="";
+    if($i>0){
+        $m_para=" -m $i";
+    }
+    print O1 "treemix -i $file$m_para -root $root -o $output_dir/out_$i > $output_dir/out_$i.log\n";
+    print O2 "source('~/software/treemix/treemix-1.12/src/plotting_funcs.R')\n";
+    print O2 "pdf(file='$output_dir/out_$i.pdf')
+plot_tree('$output_dir/out_$i')
+plot_resid('$output_dir/out_$i','order.txt')
+dev.off()
+";
+}
+close O1;
+close O2;
