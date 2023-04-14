6c4aee183e1170c9224343e8b54d24d502ef7d5e
Lulu Shi <crazzy_rabbit@163.com>
2023-04-14 21:02:00 +0800

    Delete 05_genotype.sh

--- RNA-SNPcalling/05_genotype.sh
+++
@@ -1,3 +0,0 @@
-/home/software/gatk-4.1.4.0/gatk  GenotypeGVCFs -R /home/sheep-reference/GCF_002742125.1_Oar_rambouillet_v1.0_genomic.fna \
-                                                -V combined.vcf  \
-                                                -O combined.genotype.vcf
