822d55e9034234f6cc5ff7ea624dafd5e734794b
Lulu Shi <crazzy_rabbit@163.com>
2023-04-17 21:06:02 +0800

    Update 02_01Cal-absoluteCNV.sh

--- CNV/CNVcaller/02_01Cal-absoluteCNV.sh
+++ CNV/CNVcaller/02_01Cal-absoluteCNV.sh
@@ -13,11 +13,11 @@
 CNVReferenceDBpl="/home/sll/miniconda3/CNVcaller/bin/CNVReferenceDB.pl"                  #change as you want
 IndividualProcesssh="/home/sll/miniconda3/CNVcaller/Individual.Process.sh"               #change as you want
 genomicfa="/home/sll/genome-cattle/ARS-UCD1.2/GCF_002263795.1_ARS-UCD1.2_genomic.fna"    #Reference genome fa file, change as you want
-Winsizelink="/home/sll/miniconda3/CNVcaller/Btau5.0.1_800_link"                          #dup file that you have created use blasr, change as you want
+Winsizelink="/home/sll/genome-cattle/CNVCaller-Duplink/ARS-UCD1.2_1000.link"             #dup file that you have created use blasr, change as you want
 
 winsize=1000
 echo "CNVReferenceDB.pl:    $CNVReferenceDBpl";
-echo "genomic.fna:    $genomicfna";
+echo "genomic.fna:    $genomicfa";
 echo "Individual.Process.sh:    $IndividualProcesssh";
 echo "winsize_link:    $Winsizelink";
 echo "Winsize:    $winsize"
@@ -29,4 +29,4 @@ perl $CNVReferenceDBpl $genomicfa -w $winsize
 ls *markdup.bam|cut -d"." -f 1 | sort -u | while read id;
 do
     bash $IndividualProcesssh -b `pwd`/${id}.sorted.addhead.markdup.bam -h $id -d $Winsizelink -s none;
-done    
+done 
