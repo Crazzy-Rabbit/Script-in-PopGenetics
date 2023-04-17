979f99d11eba85585b87a11e533200d4b28870d5
Lulu Shi <crazzy_rabbit@163.com>
2023-04-17 10:21:11 +0800

    Create anno.CNVcallerout.sh

---
+++ CNV/CNVcaller/anno.CNVcallerout.sh
@@ -0,0 +1,7 @@
+python /home/sll/miniconda3/CNVcaller/CNVcaller_tools/cnvcallertools/main.py annannovar --cnvfile genotypeCNVR.tsv \
+                                                                                        --outprefix anno.genotypeCNVR.vcf \
+                                                                                        --annovar /home/sll/software/annovar/annotate_variation.pl \
+                                                                                        --buildver cattle1.2 \
+                                                                                        --buildpath /home/sll/software/annovar/Bos
+                                                                                        
+                                                                                        
