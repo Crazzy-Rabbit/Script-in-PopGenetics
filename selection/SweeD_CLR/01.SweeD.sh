4958092d85c75394e9a3acc84aeb60fae45a1baa
Lulu Shi <crazzy_rabbit@163.com>
2023-04-21 10:46:34 +0800

    Update 01.SweeD.sh

--- selection/SweeD_CLR/01.SweeD.sh
+++ selection/SweeD_CLR/01.SweeD.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-if [ $# -ne 5 ]; then
+if [ $# -ne 4 ]; then
  echo "error.. need args"
- echo "command:$0 <VCF> <Chr> <Pop> <Out> <threads>"
+ echo "command: bash $0 <VCF> <Pop> <Out> <threads>"
  exit 1
 fi
+SweeD="/home/ywf/software/sweed/SweeD"
 VCF=$1
-Chr=$2
-Pop=$3
-Out=$4
-Threads=$5
-vcftools --gzvcf ${VCF} --chr ${Chr} --recode --out ${Chr}
-Sites=$[`grep -v "#" ${Chr}.recode.vcf|wc -l`/10]
+Pop=$2
+Out=$3
+Threads=$4
+
+for chr in {1..29};
+do
+vcftools --gzvcf ${VCF} --chr ${chr} --recode --out ${chr}
+Sites=$[`grep -v "#" ${chr}.recode.vcf|wc -l`/10]
 #echo ${Sites}
-SweeD-P -name ${Out} -input ${Chr}.recode.vcf -grid ${Sites} -folded -sampleList ${Pop} -threads ${Threads}
+$SweeD -name ${Out} -input ${chr}.recode.vcf -grid ${Sites} -folded -sampleList ${Pop} -threads ${Threads}
+done
