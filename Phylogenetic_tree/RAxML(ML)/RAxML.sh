fdfca0bac8cdea159314ffef6359fed41e1fdcae
Lulu Shi <crazzy_rabbit@163.com>
2023-04-23 09:22:43 +0800

    Update RAxML.sh

--- Phylogenetic_tree/RAxML(ML)/RAxML.sh
+++ Phylogenetic_tree/RAxML(ML)/RAxML.sh
@@ -2,8 +2,12 @@
 #! /bin/bash
 
 if [ $# -ne 4 ]; then
-    echo "error.. need args"
-    echo "command: $0 <Input file [vcffile]> <Outgroup> <Prefix of output> <Threads [number]>"
+    echo "command:  $0 <vcf> <Outgroup> <output> <Threads>"
+    echo "vcf:      vcf文件"
+    echo "Outgroup: 外群名称，或是外群那个个体ID"
+    echo "output:   输出文件前缀"
+    echo "Threads:  线程数，一般可设置为30"
+    echo "命令默认为bootstrap 100次，可通过对 -# 指令修改二改变次数"
     exit 1
 fi
 vcf=$1
