138661215a77246f83d4405f57bb3ae6a57ec680
Lulu Shi <crazzy_rabbit@163.com>
2023-04-23 11:10:10 +0800

    Update QC_LD_filter.sh

--- QC_LD_filter.sh
+++ QC_LD_filter.sh
@@ -1,25 +1,46 @@
 #! /bin/bash
+# 进行QC和LD过滤的命令行
+usage() {
+    echo "Usage: bash $0 --bfile <bfile> --chr <chr> --geno <geno> --maf <maf> --win <winsize> --step <step> --rr <R2>"
+    echo "required options"
+      echo "-b|--bfile       二进制plink格式文件的前缀"
+      echo "-c|--chr         物种染色体条数"
+      echo "-g|--geno        缺失率大于这个值的过滤，一般为0.05或0.1"
+      echo "-m|--maf         最小等位基因频率小于这个值的过滤，一般为0.03"
+      echo "-w|--win         窗口大小，单位为Kb"
+      echo "-s|--step        步长大小，单位为Kb"
+      echo "-r|--rr          R2阈值"
+      exit 1;
+}
 
-if [ $# -ne 7 ]; then
-    echo "command:  bash $0 <bfile> <chr> <geno> <maf> <winsize> <step> <R2>"
-    echo "bfile:    二进制plink格式文件的前缀"
-    echo "chr：     物种染色体条数"
-    echo "geno:     缺失率大于这个值的过滤，一般为0.05或0.1"
-    echo "maf:      最小等位基因频率小于这个值的过滤，一般为0.03"
-    echo "winsize:  窗口大小，单位为Kb"
-    echo "step:     步长大小，单位为Kb"
-    echo "R2:       R2阈值"
-    echo "maf可以设置为所有样品中每个位点最少存在3个等位的突变"
-    echo "一般我们设置后三个参数为 50 25 0.2"
-    exit 1
-fi
-bfile=$1
-chr=$2
-geno=$3
-maf=$4
-win=$5
-step=$6
-r=$7
+# 解析命令行参数
+OPT=`getopt -o b:c:g:m:w:s:r: --long bfile:,chr:,geno:,maf:,win:,step:,rr: -- "$@"`
+if [ $? != 0 ]; then usage; exit 1; fi
+eval set -- "OPTS"
+while [[ $# -gt 0 ]]
+do
+  key="$1"
+  case "$key" in
+    -b|--bfile )
+        bfile=$2 ; shift 2 ;;
+    -c|--chr )
+        chr=$2 ; shift 2 ;;
+    -g|--geno )
+        geno=$2 ; shift 2 ;;
+    -m|--maf )
+        maf=$2 ; shift 2 ;;
+    -w|--win )
+        win=$2 ; shift 2 ;;
+    -s|--step )
+        step=$2 ; shift 2 ;;
+    -r|--rr )
+        rr=$2 ; shift 2 ;;
+     *) echo "Option error!";
+       usage
+       shift     
+       ;;
+  esac
+done
 
 # 过滤geno和maf
 plink --allow-extra-chr --chr-set $chr \
