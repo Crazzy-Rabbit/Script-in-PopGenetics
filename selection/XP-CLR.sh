e2b9af2feb46fe618645976760741010558a40bc
Lulu Shi <crazzy_rabbit@163.com>
2023-04-23 21:41:34 +0800

    Update XP-CLR.sh

--- selection/XP-CLR.sh
+++ selection/XP-CLR.sh
@@ -4,35 +4,65 @@
 vcftools="/home/sll/miniconda3/bin/vcftools"
 xpclr="/home/sll/miniconda3/bin/xpclr"
 
-if [ $# -ne 5 ]; then 
-    echo "error.. need args"
-    echo "command: bash $0 <vcf> <ref> <tag> <winsize> <step>"
-    echo "vcf:     prefix of vcf file"
-    echo "ref:     ref sample list per row per ID"
-    echo "tag:     tag sample list per row per ID"
-    echo "winsize: winsize for xpclr"
-    echo "step:    step size for xpclr"
-    exit 1
-fi
-vcf=$1            # big vcffile
-ref=$2                            # ref sample list per row per ID
-tag=$3                           # tag sample list per row per ID
-winsize=$4                                  # winsize
-step=$5                                    # stepsize
+function usage() {
+    echo "Usage: bash $0 --vcf <vcf> --ref <ref> --tag <tag> --win <winsize> --step <step> --out <outprefix>"
+    echo "required options"
+      echo "-v|--vcf     vcf file"
+      echo "-r|--ref     ref sample list per row per ID"
+      echo "-t|--tag     tag sample list per row per ID"
+      echo "-w|--win     winsize for xpclr, default 50000"
+      echo "-s|--step    step size for xpclr, default 25000"
+      echo "-o|--out     输出文件前缀"
+      exit 1;
+}
 
+vcf=""
+ref=""
+tag=""
+win="50000"
+step="25000"
+out=""
+
+while [[ $# -gt 0 ]]
+do
+  case "$1" in
+    -v|--vcf )
+        vcf=$2 ; shift2 ;;
+    -r|--ref )
+        ref=$2 ; shift2 ;;
+    -t|--tag )
+        tag=$2 ; shift2 ;;
+    -w|--win )
+        win=$2 ; shift2 ;;
+    -s|--step )
+        step=$2 ; shift2 ;;
+    -o|--out )
+        out=$2 ; shift2 ;;
+    *) echo "Option error!" ;
+        usage
+        shift
+        ;;
+  esac
+done
+
+if [ -z $vcf ] || [ -z $ref ] || [ -z $tag ] || [ -z $out ]; then
+    echo "option --vcf and --ref and --tag and --out not specified" >&2
+    usage
+fi
+        
+function main {
 mkdir XP-CLR.progress
 cd XP-CLR.progress
-win=$((winsize/1000))  
 
 for k in {1..29};
 do
-$vcftools --vcf ../${vcf}.vcf \
+$vcftools --vcf $vcf \
           --recode --recode-INFO-all \
           --chr ${k} \
-          --out ${vcf}.chr${k}
+          --out ${out}.chr${k}
           
 # calculate map distance                
-$vcftools --vcf ${vcf}.chr${k}.recode.vcf \
+$vcftools --vcf ${out}.chr${k}.recode.vcf \
           --plink \
           --out chr${k}.MT
 awk 'BEGIN{OFS=" "} {print 1,".",$4/1000000,$4}' chr${k}.MT.map > chr${k}.MT.map.distance
@@ -42,18 +72,19 @@ for k in {1..29};
 do
 # xpclr
 xpclr --out chr${k} --format vcf \
-      --input ${vcf}.chr${k}.recode.vcf \
+      --input ${out}.chr${k}.recode.vcf \
       --samplesA $ref \
       --samplesB $tag \
       --map chr${k}.MT.map.distance \
       --chr ${k} \
       --gdistkey None --phased \
-      --size $winsize --step $stepsize
+      --size $win \
+      --step $step
 
 # merge
-awk  '{print $2,$3,$4,$12,$13}'   Chr${k} > Chr${k}.ref_tag.chart.xpclr.${win}kb.windows
+awk  '{print $2,$3,$4,$12,$13}'   Chr${k} > Chr${k}.${out}
 done
 
-echo "XP-CLR has been finished!"
-
-cat ./*.ref_tag.chart.xpclr.${win}kb.windows > ../all.xpclr.${win}kb.windows
+cat ./*.${out} > ../${out}.xpclr
+}
+main
