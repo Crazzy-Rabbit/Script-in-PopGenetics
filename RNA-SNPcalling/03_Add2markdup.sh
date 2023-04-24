97d88e22b45e68dc48026c395b3ffd713264040b
Lulu Shi <crazzy_rabbit@163.com>
2023-04-24 11:49:22 +0800

    Update 03_Add2markdup.sh

--- RNA-SNPcalling/03_Add2markdup.sh
+++ RNA-SNPcalling/03_Add2markdup.sh
@@ -2,16 +2,16 @@
 
 picard="/home/software/picard/build/libs/picard.jar"
 
-ls *markdup.bam | cut -d"." -f 1 | sort -u  | while read id;
+ls *filter.fastq.gz | cut -d '_' -f 1 | sort -u | while read id;
 do
   # 1. picard加头排序
   java -jar -Xmx15g $picard AddOrReplaceReadGroups \
-                       I=${id}_sort.bam \
+                       I=${id}_2passAligned.out.sam \
                        O=./sample_RNA_SNP_calling/${id}_sort_added.bam \
                        SO=coordinate \
                        RGID=${id} RGLB=mRNA RGPL=illumina RGPU=NovaSeq6000 RGSM=${id} #按照自己测序的来
   # 2. 标记重复
-  java -jar $picard MarkDuplicates \
+  java -jar -Xmx15g $picard MarkDuplicates \
                        I=${id}_sort_added.bam \
                        O=${id}.sorted.addhead.markdup.bam \
                        M=${id}.sorted.addhead.markdup.metrics.txt \
