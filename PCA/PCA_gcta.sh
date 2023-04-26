610bd93dba8b2ff105435bcc7b8a940d905c3bb8
Lulu Shi <crazzy_rabbit@163.com>
2023-04-26 21:55:32 +0800

    Update PCA_gcta.sh

--- PCA/PCA_gcta.sh
+++ PCA/PCA_gcta.sh
@@ -1,12 +1,53 @@
-bfile="QC.common_89_cattle_851_ASIA-geno005-maf003"
+#! /bin/bash
+# PCA
+gcta="/home/software/gcta_1.92.3beta3/gcta64"
+function usage() {
+    echo "Usage:"
+    echo "required options"
+      echo "-b|--bfile     bfile文件前缀"
+      echo "-a|--autosome  常染色体数量，默认为 29"
+      echo "-p|--pca       计算的pc个数， 默认为 4"
+      echo "-o|--out       输出文件前缀"
+    exit 1;
+}
 
+bfile=""
+autosome="29"
+pca="4"
+out=""
+
+while [[ $# -gt 0 ]]
+do 
+  case "$1" in
+    -b|--bfile )
+      bfile=$2 ; shift2 ;;
+    -a|--autosome )
+      autosome=$2 ; shift2 ;;
+    -p|--pca )
+      pca=$2 ; shift2 ;;
+    -o|--out )
+      out=$2 ; shift2 ;;
+    *) echo "没有这个参数！" >&2
+      usage
+      shift
+      ;;
+  esac
+done
+
+if [ -z $bfile ] || [ -z $out ]; then 
+    echo "输入和输出文件未指定！" >&2
+    usage
+fi
+
+function main() {
 ## make germ
-/home/software/gcta_1.92.3beta3/gcta64 --bfile $bfile \
-                                       --make-grm --autosome-num 29 \
-                                       --out $bfile.gcta
+$gcta --bfile $bfile \
+      --make-grm --autosome-num $autosome \
+      --out ${out}.gcta
                                        
 ## PCA
-/home/software/gcta_1.92.3beta3/gcta64 --grm $bfile.gcta \
-                                       --pca 4 \
-                                       --out $bfile.gcta.out
-                                       
+$gcta --grm ${out}.gcta \
+      -pca $pca \
+      --out ${out}.gcta.out
+}
+main
