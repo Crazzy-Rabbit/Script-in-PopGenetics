0bed59d89275242ba0bd010b5f36d1b51577069a
Lulu Shi <crazzy_rabbit@163.com>
2023-05-07 14:17:06 +0800

    Update 07_plot-bubble.R

--- RNA/RNA-DEG/07_plot-bubble.R
+++ RNA/RNA-DEG/07_plot-bubble.R
@@ -4,21 +4,18 @@
 
 library(ggplot2)
 library(openxlsx)
-goinput <- read.xlsx("go.xlsx")                                   # 载入GO富集分析的结果文件
 
+goinput <- read.xlsx("go.xlsx")                                   # 载入GO富集分析的结果文件
 x=goinput$GeneRatio                                               # 设置以哪个数据为X轴
 y=factor(goinput$Description,levels = goinput$Description)        # 设置Y轴
-
-p = ggplot(goinput,aes(x,y))                                      # 开始以X轴和Y轴绘制画布
+p  = ggplot(goinput,aes(x,y))                                      # 开始以X轴和Y轴绘制画布
 p1 = p + geom_point(aes(size=Count,color=-0.5*log(pvalue),shape=ONTOLOGY,))+
-  scale_color_gradient(low = "SpringGreen", high = "OrangeRed")
-p1
-
-p1 + labs(color=expression(-log[10](pvalue)),
+         scale_color_gradient(low = "SpringGreen", high = "OrangeRed")
+p2 = p1 + labs(color=expression(-log[10](pvalue)), 
                size="Count",
-               x="GeneRatio",
-               y="Go_term",
+               x="GeneRatio", y="Go_term",
                title="Go enrichment of test Genes")
+p2
 
 # KEGG气泡图
 library(ggplot2)
@@ -27,14 +24,11 @@ library(openxlsx)
 kegginput <- read.xlsx("kegg.xlsx")
 x=kegginput$pvalue
 y=factor(kegginput$Term,levels = kegginput$Term)
-
-p = ggplot(kegginput,aes(x,y))
+p  = ggplot(kegginput,aes(x,y))
 p1 = p + geom_point(aes(size=Count,color=-0.5*log(pvalue)))+
-  scale_color_gradient(low = "BLUE", high = "OrangeRed")          # 以颜色的由暗蓝到橙红代表P值的对数值
-
+         scale_color_gradient(low = "BLUE", high = "OrangeRed")  # 以颜色的由暗蓝到橙红代表P值的对数值
 p2 = p1 + labs(color=expression(-log[10](pvalue)),
                size="Count",
-               x="P value",
-               y="KEGG",
+               x="P value", y="KEGG",
                title="KEGG pathway of Target Genes")
 p2
