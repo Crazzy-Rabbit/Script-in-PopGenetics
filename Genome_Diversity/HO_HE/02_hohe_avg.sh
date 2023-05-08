acca52dce6c25178b2f1388875a603d1bccb93fb
Lulu Shi <crazzy_rabbit@163.com>
2023-05-08 15:09:01 +0800

    Update and rename Genome_Diversity/hohe_avg.sh to Genome_Diversity/HO_HE/02_hohe_avg.sh

--- Genome_Diversity/HO_HE/02_hohe_avg.sh
+++ Genome_Diversity/HO_HE/02_hohe_avg.sh
@@ -1,11 +1,11 @@
 for i in Angus Holstein Simmental Shorthorn Charolais Hanwoo JPBC Mishima-Ushi Kazakh Mongolian Yanbian Wenling Brahman;
-do cat  ${i}.hwe|awk '{sum+=$7} END {print "'"${i}"'", sum/NR}' >  ${i}.HO;
-cat  ${i}.hwe|awk '{sum+=$8} END {print sum/NR}'  >  ${i}.HE;
-
+do 
+  cat  ${i}.hwe|awk '{sum+=$7} END {print "'"${i}"'", sum/NR}' >  ${i}.HO;
+  cat  ${i}.hwe|awk '{sum+=$8} END {print sum/NR}'  >  ${i}.HE;
 done
+
 cat *.HO > HO
 cat *.HE > HE
-
 paste HO HE > hohe.txt
 
 sed -i 's/ /\t/g' hohe.txt
