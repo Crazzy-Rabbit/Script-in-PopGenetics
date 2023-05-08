b464731ed2c51881ebd1dde933322cd8c7c45ac9
Lulu Shi <crazzy_rabbit@163.com>
2023-05-08 19:28:25 +0800

    Update and rename plot_PCA.R to plot_PCA.R

--- 02.Pop_Genome/PCA/plot_PCA.R
+++ 02.Pop_Genome/PCA/plot_PCA.R
@@ -10,23 +10,18 @@ library("ggplot2")
 Breed=a[,1] 
 
 p = ggplot(data  = a , 
-           aes(x = a[,3],
-               y = a[,4],   ############x = a[,3], y = a[,4]代表第3列和第4列比较也就是pc1与pc2比较
-               group = Breed,
-               shape = Breed,
-               stroke = 1.5))+
+           aes(x = a[,3], y = a[,4],   ############x = a[,3], y = a[,4]代表第3列和第4列比较也就是pc1与pc2比较
+               group = Breed,shape = Breed,stroke = 1.5))+
   geom_point(aes(color=Breed),size=3) +
-  scale_shape_manual(values = seq(1,15)) + ##设置形状
-  # scale_shape_manual(values = rep(12,times = 13)) + 
+  scale_shape_manual(values = seq(1,15)) + ##设置形状 # scale_shape_manual(values = rep(12,times = 13)) + 
   guides(color=guide_legend(override.aes = list(size=4, stroke = 1.5))) ##改变图例大小
 
-  p + labs(x = "PC1(12.44%)", y = "PC2(5.87%)") + ############修改x轴和y轴的坐标名称
-    
+  p + labs(x = "PC1(12.44%)", y = "PC2(5.87%)") + ############修改x轴和y轴的坐标名称   
   scale_color_manual(values=c("#66a61e", "#66a61e","#66a61e","#66a61e","#66a61e",
                                 "#7470b3", "#e7288a","#e728e7","#e7288a","#e7288a","#e7288a",
                                 "#b35107","#b35107")) +   
-    theme_classic()+ # 去除灰色背景及网格线
-    theme(panel.border = element_rect(fill=NA,color="black", size=0.5, linetype ="solid")) #添加边框
+  theme_classic()+ # 去除灰色背景及网格线
+  theme(panel.border = element_rect(fill=NA,color="black", size=0.5, linetype ="solid")) #添加边框
 
 
   ##计算方差解释度
