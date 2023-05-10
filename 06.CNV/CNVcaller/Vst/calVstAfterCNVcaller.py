7ce06dc2904c09a8d97dbd966fc0169146f6ea28
Lulu Shi <crazzy_rabbit@163.com>
2023-05-10 16:04:56 +0800

    Update calVstAfterCNVcaller.py

--- 06.CNV/CNVcaller/Vst/calVstAfterCNVcaller.py
+++ 06.CNV/CNVcaller/Vst/calVstAfterCNVcaller.py
@@ -5,19 +5,6 @@ Created on  05 10 10:39:56  2023
 @Author : Lulu Shi
 @Mails : crazzy_rabbit@163.com
 """
-"""
-CNVFilterAfterCNVcaller.py过滤后，对mergeCNVR文件提取后的文件
-计算公式为：
-VST =[Vt − (V1×N1+V2×N2)Nt]/Vt
-
-V1和V2分别为两群体各自的拷贝数方差，
-Nt为样本量总和，Vt为总的拷贝数方差
-
-简化为：VST =[Vt − Vs]/Vt
-Vs为群体各自拷贝数方差加权群体大小
-Nt为样本量总和，Vt为总的拷贝数方差
-参考：An atlas of CNV maps in cattle, goat and sheep
-"""
 import click
 import pandas as pd
 
@@ -47,9 +34,7 @@ def target(poplist, infile):
 def main(file, pop1, pop2, out):
     """
     CNVFilterAfterCNVcaller.py过滤后，GetCleanCNV.py提取后的文件
-    计算公式为：
-    VST =[Vt − (V1×N1+V2×N2)Nt]/Vt
-
+    计算公式为: VST =[Vt − (V1×N1+V2×N2)Nt]/Vt
     V1和V2分别为两群体各自的拷贝数方差，
     Nt为样本量总和，Vt为总的拷贝数方差
 
