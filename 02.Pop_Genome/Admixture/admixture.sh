ea3f6e955d795a56fff2ead3fdd61b9ae99265ff
Lulu Shi <crazzy_rabbit@163.com>
2023-05-21 12:07:31 +0800

    Update admixture.sh

--- 02.Pop_Genome/Admixture/admixture.sh
+++ 02.Pop_Genome/Admixture/admixture.sh
@@ -2,31 +2,26 @@
 
 if [ $# -ne 3 ]; then
   echo "error.. need args"
-  echo "command: bash $0 <mK, 最大的K值, 默认从2开始> <nseed, 1-100内随机数的数量，推荐20> <bed file, bed文件>"
+  echo "command: bash $0 <mK, 最大的K值, 默认从2开始> <nseed, 1-100内随机数的数量，推荐20> <bed file, bed文件,要绝对路径>"
   exit 1
 fi
 
 nK=$1
 nseed=$2
 bed=$3
+base_dir="`pwd`/admixture"   # 存储输出结果的基本目录
+
+mkdir -p $base_dir
 
 # 生成20个随机数
-for i in $(seq 2 $nK); 
+for r in $(seq 1 $nseed); 
 do 
-  for j in $(shuf -i 1-100 -n $nseed); 
-  do 
-    echo $i $j;    
-  done; 
-done > seed.txt
-
-
-# do admixture
-cat seed.txt | while read line;
-do
-  para1=`echo $line | awk '{print $1}' `
-  para2=`echo $line| awk '{print $2}' `
-
-  export K=$para1
-   export seed=$para2
-  /home/software/admixture_linux-1.3.0/admixture -s $seed --cv $bed $K -j4 | tee log${K}.out
+  curr_dir="${base_dir}/${r}.run"
+  mkdir -p $curr_dir && cd $curr_dir
+  
+  for K in $(seq 2 $nK); 
+  do
+    seed=`shuf -i 1-100 -n 1`
+    /home/software/admixture_linux-1.3.0/admixture -s $seed --cv $bed $K -j4 | tee ${curr_dir}/log${K}.out
+  done;
 done
