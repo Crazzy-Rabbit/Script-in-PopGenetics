9889dc74278112a4538a3bc31b985774c9bf682a
Lulu Shi <crazzy_rabbit@163.com>
2023-05-23 10:53:23 +0800

    Update cal_cv.sh

--- 02.Pop_Genome/Admixture/cal_cv.sh
+++ 02.Pop_Genome/Admixture/cal_cv.sh
@@ -9,7 +9,7 @@ K=$1
 
 for i in $(seq 2 $K);
 do
-  grep -h CV *run/log${i}.out > ${i}cv_out.txt
+  grep "CV" *run/log${i}.out | awk '{print $3,$4}' | cut -c 4,7-20 > ${i}cv_out.txt
 done
 
 paste *cv_out.txt > all.cv_out
