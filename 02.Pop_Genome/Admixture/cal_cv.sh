e46a19053e7524aeff2fab4b93472fa5df5d4c87
Lulu Shi <crazzy_rabbit@163.com>
2023-05-23 14:44:04 +0800

    Update cal_cv.sh

--- 02.Pop_Genome/Admixture/cal_cv.sh
+++ 02.Pop_Genome/Admixture/cal_cv.sh
@@ -1,12 +1,5 @@
 #! /bin/bash
 
-if [[ $# -ne 1 ]]; then
-  echo "command: bash $0 <K, 最大的那个K>"
-  exit 1
-fi
-
-K=$1
-
 for i in {1..20};
 do
   grep "CV" ${i}.run/log*.out | awk '{print $3, $4}' | cut -c 4,7-20 > ${i}run.cv_out.txt
