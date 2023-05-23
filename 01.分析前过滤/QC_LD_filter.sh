854b33d499c6721abf971e86edd3da8187025be2
Lulu Shi <crazzy_rabbit@163.com>
2023-05-23 10:05:30 +0800

    Update QC_LD_filter.sh

--- 01.分析前过滤/QC_LD_filter.sh
+++ 01.分析前过滤/QC_LD_filter.sh
@@ -1,22 +1,28 @@
 #! /bin/bash
-# 进行QC和LD过滤的命令行
-#! /bin/bash
-# 进行QC和LD过滤的命令行
+
+########################################################
+# 进行geno maf 及 LD过滤，有默认参数
+# 输入文件可以为vcf，也可以为bed等plink二进制文件，全名
+# 需要指定物种的染色体条数，用于Plink中的all extra chr
+# 对重测序数据SNP ID那列为'.',进行填充，因为会影响LD过滤的点
+# @Author: Lulu Shi
+# @Email: crazzy_rabbit@163.com
+########################################################
 function usage() {
-    echo "Usage: bash $0 --bfile <bfile> --chr <chr> --geno <geno> --maf <maf> --win <winsize> --step <step> --rr <R2>"
+    echo "Usage: bash $0 --file <file> --chr <chr> --geno <geno> --maf <maf> --win <winsize> --step <step> --rr <R2>"
     echo "required options"
-      echo "-b|--bfile       二进制plink格式文件的前缀"
-      echo "-c|--chr         物种染色体条数"
-      echo "-g|--geno        缺失率大于这个值的过滤，默认为0.05"
-      echo "-m|--maf         maf小于这个值的过滤，默认为0.03"
-      echo "-w|--win         窗口大小，单位为Kb，默认为50"
-      echo "-s|--step        步长大小，单位为Kb，默认为25"
-      echo "-r|--rr          R2阈值， 默认为0.2"
+      echo "-f|--file        vcf file or bed or bim or fam file（全名）"
+      echo "-c|--chr         num of chr"
+      echo "-g|--geno        geno，default 0.05"
+      echo "-m|--maf         maf，default 0.03"
+      echo "-w|--win         winsize，单位为Kb，default 50"
+      echo "-s|--step        stepsize，单位为Kb，default 25"
+      echo "-r|--rr          R2阈值， default 0.2"
       exit 1;
 }
 
 # Set default values:
-bfile=""
+file=""
 chr=""
 geno="0.05"
 maf="0.03"
@@ -24,7 +30,6 @@ win="50"
 step="25"
 rr="0.2"
 
-# 解析命令行参数
 while [[ $# -gt 0 ]];
 do
   case "$1" in
@@ -47,29 +52,46 @@ do
   esac
 done 
 
-if [ -z "$bfile" ] || [ -z "$chr" ]; then
-    echo "OPtion --bfile and --chr must be specified." >$2
+if [ -z "$file" ] || [ -z "$chr" ]; then
+    echo "OPtion --file and --chr must be specified." >$2
     usage
 fi
 
+# prefix for geno and maf
+length="${#geno}"
+padding="$((length))"
+genoname="$(printf "%0${padding}s%s" "0" "$geno")"
+
+length="${#maf}"
+padding="$((length))"
+mafname="$(printf "%0${padding}s%s" "0" "$maf")"
+
+# use 'cut' split filename, specify prefix after geno and maf
+pre=$(echo $file | cut -d "." -f -1)
+prefix=$pre-geno${genoname}-maf${mafname}
+
+# Check if the input file is vcf file, if it is, convert the format
+if [[ "${file##*.}" = "vcf" ]]; then
+  plink --allow-extra-chr --chr-set $chr -vcf $file --make-bed --double-id --out $prefix
+fi
+
 function main() {
-# 过滤geno和maf
-plink --allow-extra-chr --chr-set $chr -bfile $bfile --geno $geno --maf $maf --make-bed --out QC.${bfile}
+# geno and maf filter
+plink --allow-extra-chr --chr-set $chr -bfile $pre --geno $geno --maf $maf --make-bed --out QC.$prefix
 
-# 过滤LD
-# 转为map、ped格式
-plink --allow-extra-chr --chr-set $chr -bfile QC.$bfile --recode --out QC.${bfile}
-# 填充map文件第二列，
-awk -F '\t' '{print $1"\t"$1":"$4"\t"$3"\t"$4}' QC.${bfile}.map > QC.${bfile}.correction.map
-# 删除原来的map文件, 将生成的map文件改为原来的map文件名称
-rm QC.${bfile}.map
-mv QC.${bfile}.correction.map QC.${bfile}.map
-#转为二进制格式
-plink --allow-extra-chr --chr-set $chr --file QC.${bfile} --make-bed --out QC.${bfile}
+# LD filter
+# convert to map ped
+plink --allow-extra-chr --chr-set $chr -bfile QC.$prefix --recode --out QC.$prefix
+# Fill the second column of map file
+awk -F '\t' '{print $1"\t"$1":"$4"\t"$3"\t"$4}' QC.${prefix}.map > QC.${prefix}.correction.map
+# change the name of the generated map file to the original map file
+rm QC.${prefix}.map
+mv QC.${prefix}.correction.map QC.${prefix}.map
+# convert to bed bim fam
+plink --allow-extra-chr --chr-set $chr --file QC.${prefix} --make-bed --out QC.${prefix}
 
-#进行LD过滤
-plink --allow-extra-chr --chr-set $chr -bfile QC.${bfile}  --indep-pairwise $win $step $rr --out QC.ld.${bfile}-502502
-
-plink --allow-extra-chr --chr-set $chr -bfile QC.${bfile} --extract QC.ld.${bfile}-502502.prune.in --make-bed --out QC.ld.${bfile}-502502
+# do LD filter
+plink --allow-extra-chr --chr-set $chr -bfile QC.${prefix}  --indep-pairwise $win $step $rr --out QC.ld.${prefix}-502502
+plink --allow-extra-chr --chr-set $chr -bfile QC.${prefix} --extract QC.ld.${prefix}-502502.prune.in --make-bed --out QC.ld.${prefix}-502502
 }
 main
