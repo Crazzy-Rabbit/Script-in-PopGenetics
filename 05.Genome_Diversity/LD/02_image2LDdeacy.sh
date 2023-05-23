c64ae14bf42cb74d7f3b7147071d7611c63b9983
Lulu Shi <crazzy_rabbit@163.com>
2023-05-23 20:20:21 +0800

    Update 02_image2LDdeacy.sh

--- 05.Genome_Diversity/LD/02_image2LDdeacy.sh
+++ 05.Genome_Diversity/LD/02_image2LDdeacy.sh
@@ -1,3 +1,4 @@
-Plot_MultiPop.pl  -inList  Pop.ResultPath.txt -bin1 500 -bin2 7000 -break 5000 -output Fig
+Plot_MultiPop.pl  -inList  Pop.ResultPath.txt -bin1 10 -bin2 100 -break 100 -output Fig
 # 制作一个第一列为上面生成群体对应的路径，第二列为群体ID的txt文件的前缀，就是图中每个线的名称
-# -bin1 -bin2 -break 可以看结果来加大，若结果不太平滑的话
+# -bin1 -bin2 -break 可以看结果来加大，若结果不太平滑的话，推荐使用默认
+# 也就是bin1 10 bin2 100 break 100
