0c7db22cbe243198571d20f3cab9e4bffba56b85
Lulu Shi <crazzy_rabbit@163.com>
2023-06-07 15:16:37 +0800

    Update 01.SweeD.sh

--- 03.selection_signature/SweeD_CLR/01.SweeD.sh
+++ 03.selection_signature/SweeD_CLR/01.SweeD.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-if [ $# -ne 4 ]; then
+if [ $# -ne 3 ]; then
  echo "error.. need args"
- echo "command: bash $0 <VCF> <Pop> <Out> <winsize>"
+ echo "command: bash $0 <VCF> <Pop> <winsize>"
  exit 1
 fi
 SweeD="/home/ywf/software/sweed/SweeD"
 VCF=$1
 Pop=$2
-Out=$3
-winsize=$4
+winsize=$3
 
 for chr in {1..29};
 do
 vcftools --gzvcf ${VCF} --chr ${chr} --recode --out ${chr}
 Sites=$[`grep -v "#" ${chr}.recode.vcf|wc -l`/$winsize]
 #echo ${Sites}
-$SweeD -name ${Out} -input ${chr}.recode.vcf -grid ${Sites} -folded -sampleList ${Pop}
+$SweeD -name $chr -input ${chr}.recode.vcf -grid ${Sites} -folded -sampleList ${Pop}
 done
