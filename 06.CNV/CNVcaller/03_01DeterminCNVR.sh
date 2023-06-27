91ece0b8c4bef779572053e897ef5ce8a82dbe27
Lulu Shi <crazzy_rabbit@163.com>
2023-06-27 20:51:12 +0800

    Update and rename 03_CNVR2Genotype.sh to 03_01DeterminCNVR.sh

--- 06.CNV/CNVcaller/03_01DeterminCNVR.sh
+++ 06.CNV/CNVcaller/03_01DeterminCNVR.sh
@@ -1,9 +1,7 @@
-# Set up the file name(obtain the absolute paths), software                                             
+#! /bin/bash
+## determin the CNVR 
 
 CNVDiscoverysh="/home/sll/miniconda3/CNVcaller/CNV.Discovery.sh"                         #change as you want
-Genotypepy="/home/sll/miniconda3/CNVcaller/Genotype.py"                                  #change as you want
-python="/home/sll/miniconda3/bin/python3.9"
-
 
 cp referenceDB.${winsize} RD_normalized
 cd RD_normalized
@@ -16,8 +14,3 @@ bash $CNVDiscoverysh -l `pwd`/list.txt -e `pwd`/exclude_list -f 0.1 -h 3 -r 0.5 
 # -f  在一个拷贝数窗口中这一类型拷贝数的最小频率
 # -h  定义拷贝数窗口时，同类型的拷贝数个体大于这个数，则为这一类拷贝数窗口
 # -r （两个非重叠窗口K间的最小泊松相关系数）0.5 样本量在30个以内； 0.4 样本量在30-50； 0.3 样本量在50-100； 0.2 样本量在100-200
-
-
-# Genotype determination
-$python $Genotypepy --cnvfile mergeCNVR --outprefix genotypeCNVR --nproc 8
-echo "Congratulation!CNVCaller has finished now!"
