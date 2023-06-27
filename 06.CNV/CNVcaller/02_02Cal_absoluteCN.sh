364b125cf48344ccd8e2603eafcb39a768e35f7f
Lulu Shi <crazzy_rabbit@163.com>
2023-06-27 20:44:00 +0800

    Update 02_02Cal_absoluteCN.sh

--- 06.CNV/CNVcaller/02_02Cal_absoluteCN.sh
+++ 06.CNV/CNVcaller/02_02Cal_absoluteCN.sh
@@ -1,5 +1,5 @@
 IndividualProcess="/home/sll/miniconda3/CNVcaller/Individual.Process.sh"               #change as you want
-Winlink="/home/sll/genome-cattle/CNVCaller-Duplink/ARS_UCD1.2-refenenceDB.1000"        #dup file that you have created use blasr, change as you want
+Winlink="/home/sll/genome-cattle/CNVCaller-Duplink/ARS_UCD1.2_1000_link"               #dup file that you have created use blasr, change as you want
 
 # Calculate the absolute copy number  of each window
 ls *markdup.bam|cut -d"." -f 1 | sort -u | while read id;
