30c24823deaacf6069a9d85c73c7dd16f5345319
Lulu Shi <crazzy_rabbit@163.com>
2023-06-27 20:47:38 +0800

    Update 02_02Cal_absoluteCN.sh

--- 06.CNV/CNVcaller/02_02Cal_absoluteCN.sh
+++ 06.CNV/CNVcaller/02_02Cal_absoluteCN.sh
@@ -1,3 +1,6 @@
+#! /bin/bash
+## cal absolute copy number
+
 IndividualProcess="/home/sll/miniconda3/CNVcaller/Individual.Process.sh"               #change as you want
 Winlink="/home/sll/genome-cattle/CNVCaller-Duplink/ARS_UCD1.2_1000_link"               #dup file that you have created use blasr, change as you want
 
