51817f09a5170e187e5de4a47bb06f79987e0833
Lulu Shi <crazzy_rabbit@163.com>
2023-06-30 14:32:10 +0800

    Update 01_fastp-hisat2-featurecount.sh

--- 07.RNA/RNA-DEG/01_fastp-hisat2-featurecount.sh
+++ 07.RNA/RNA-DEG/01_fastp-hisat2-featurecount.sh
@@ -19,7 +19,6 @@ multiqc="/home/sll/miniconda3/bin/multiqc"
 genomefa="/home/sll/genome-cattle/ARS-UCD1.2/GCF_002263795.1_ARS-UCD1.2_genomic"                   # change as you want
 genomegtf="/home/sll/genome-cattle/ARS-UCD1.2/GCF_002263795.1_ARS-UCD1.2_genomic.gtf"              # change as you want 
 
-
 mkdir hismap.sam
 # ensure your sra file was xxx.sra format
 ls *sra | while read id;
@@ -29,22 +28,16 @@ sample=${arr[0]}
 
 # 1 sra to fq_1 and fq_2
 $fastq-dump --gzip --split-3 $sample
-
 # 2 QC use fastp
 $fsatp -i ${sample}_1.fastq.gz -I ${sample}_2.fastq.gz -g -q -n 15 -l 150 -u 50 -o ${sample}_1.filter.fastq.gz -O ${sample}_2.filter.fastq.gz -h ${sample}.fastp.html
-
 # 3 reads mapping  ----hisat2
-$hisat2 -p 8 -x $genomefa -1 ${sample}_1.filter.fastq.gz -2 ${sample}_2.filter.fastq.gz -S hismap.sam/${sample}.hismap.sam
-       
+$hisat2 -p 8 -x $genomefa -1 ${sample}_1.filter.fastq.gz -2 ${sample}_2.filter.fastq.gz -S hismap.sam/${sample}.hismap.sam       
 # 4 sam to bam and sorted and index for *sort.bam file
-cd ./hismap.sam
-$samtools view -S ${sample}.hismap.sam -b | samtools sort -@ 8 -o ${sample}_sort.bam | samtools index - ${sample}_sort.bam.index
-cd ..
+$samtools view -S ./hismap.sam/${sample}.hismap.sam -b | samtools sort -@ 8 -o ${sample}_sort.bam | samtools index - ./hismap.sam/${sample}_sort.bam.index
 done
 
-cd ./hismap.sam
 # 5 featurecount count the reads number
-$featureCounts -p -t -g gene_id -M -T 8 -a $genomegt -o ../all.featurecounts.txt ./*_sort.bam
+$featureCounts -p -t -g gene_id -M -T 8 -a $genomegt -o all.featurecounts.txt ./hismap.sam/*_sort.bam
 cd ..
 # 6 multiqc 
 $multiqc all.featurecounts.txt.summary -o  all.counts.summary
