5c1a02b8e7764d373e6b00ba8c4c3b2d00a7eda1
Lulu Shi <crazzy_rabbit@163.com>
2023-07-01 15:51:56 +0800

    Update Hard_filter_for_SNP.sh

--- 01.分析前过滤/Hard_filter_for_SNP.sh
+++ 01.分析前过滤/Hard_filter_for_SNP.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 ## 精准而优雅
 #################### Hard filter for SNP ######################
-
+# 如果想要位点多点，则最后--minQ可不设置
+# vcftools --non-ref-ac 2  保留非参考等位基因多余2个个体的位点
+# 看自己情况设还是不设
+###############################################################
 # Set up the file name, software
-GATK="/home/software/gatk-4.1.4.0/gatk"                #change as you want
-vcftools="/home/sll/miniconda3/bin/bcftools"           #change as you want
-reference="/home/sll/genome"
+GATK="/home/software/gatk-4.1.4.0/gatk"                # change as you want
+vcftools="/home/sll/miniconda3/bin/bcftools"           # change as you want
+reference="/home/sll/genome"                           # reference genome fasta file
 
 if [[ $# -ne 2 ]]; then 
     echo "Usage: bash $0  <vcf.gz file>  <outprefix>"
@@ -25,7 +28,7 @@ $GATK VariantFiltration -R $reference -V ${out}.snps.vcf.gz --filter-expression 
 $GATK VariantFiltration -R $reference -V ${out}.snps.vcf.gz --filter-expression "QD < 2.0 || FS > 200.0 || SOR > 10.0 || MQRankSum < -12.5 || ReadPosRankSum < -20" --filter-name "INDEL_FILTER" -O ${out}.indel.HDflt.vcf.gz
 ## get pass for SNP and INDEL
 $GATK MergeVcfs -I ${out}.snps.HDflt.vcf.gz -I ${out}.indel.HDflt.vcf.gz -O ${out}.SNP.INDEL.HDflt.vcf.gz
-$GATK SelectVariants -R $reference.fa -V ${out}.SNP.INDEL.HDflt.vcf.gz -O ${out}.SNP.INDEL.HDflt.pass.vcf.gz -select "vc.isNotFiltered()"
+$GATK SelectVariants -R $reference -V ${out}.SNP.INDEL.HDflt.vcf.gz -O ${out}.SNP.INDEL.HDflt.pass.vcf.gz -select "vc.isNotFiltered()"
 
 ## get SNP and indel
 $vcftools --gzvcf ${out}.SNP.INDEL.HDflt.pass.vcf.gz --minQ 30 --min-alleles 2 --max-alleles 2 --remove-indels --recode --recode-INFO-all --out ${out}.SNP.HDflt.pass
