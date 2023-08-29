46d4c4fa85711472bd8150f7ab7836debabb659d
Lulu Shi <crazzy_rabbit@163.com>
2023-08-29 10:30:23 +0800

    Update and rename treemix.sh to 02_treemix.sh

--- 02.Gene_flow/Treemix/02_treemix.sh
+++ 02.Gene_flow/Treemix/02_treemix.sh
@@ -2,4 +2,5 @@
 
 plink2treemixIN="/home/sll/script/treemix/plink2treemixIN.py"
 
+
 python $plink2treemixIN --sample 157_cattle_snp_geno01_maf005-ld502502_nchr.order.txt --bed /home/sll/20230818-sll-vcf/structure/NJtree/159_cattle_snp_geno01_maf005 --output tree
