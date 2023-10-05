550406e5b33bbe6768836295fd64f52bc8732dcd
Lulu Shi <crazzy_rabbit@163.com>
2023-10-05 19:38:14 +0800

    Update TreatQTLdbfile.py

--- TreatQTLdbfile.py
+++ TreatQTLdbfile.py
@@ -14,25 +14,25 @@ import click
 def main(QTLdb,out):
     """
     将从QTL数据库下载下来的bed格式的文件换成下列格式，并调整有些位置
+
     Chr.X	741365	741369	Bovine respiratory disease susceptibility QTL (57587)
     Chr.X	1626461	1626465	Interval to first estrus after calving QTL (30110)
     Chr.X	1626461	1626465	Interval to first estrus after calving QTL (30331)
     """
-    with open (QTLdb, 'r') as f:
-        for line in f:
-            if line.startswith('#'):
-                continue
-            elif line.startswith('Chr'):
-                Chr = line.strip().split("\t")[0]
-                tempStart = line.strip().split("\t")[1]
-                tempEnd = line.strip().split("\t")[2]
-                QTL = line.strip().split("\t")[3]
-                if tempStart < tempEnd:
-                    out.write(f'{Chr}\t{tempStart}\t{tempEnd}\t{QTL}\n')
-                elif tempStart > tempEnd:
-                    out.write(f'{Chr}\t{tempStart}\t{tempEnd}\t{QTL}\n')
-                else:
-                    out.write(f'{Chr}\t{tempStart}\t{tempEnd}\t{QTL}\n')
+    for line in QTLdb:
+        if line.startswith('#'):
+            continue
+        elif line.startswith('Chr'):
+            Chr = line.strip().split("\t")[0]
+            tempStart = line.strip().split("\t")[1]
+            tempEnd = line.strip().split("\t")[2]
+            QTL = line.strip().split("\t")[3]
+            if tempStart < tempEnd:
+                out.write(f'{Chr}\t{tempStart}\t{tempEnd}\t{QTL}\n')
+            elif tempStart > tempEnd:
+                out.write(f'{Chr}\t{tempStart}\t{tempEnd}\t{QTL}\n')
+            else:
+                out.write(f'{Chr}\t{tempStart}\t{tempEnd}\t{QTL}\n')
 
 if __name__ == '__main__':
     main()
